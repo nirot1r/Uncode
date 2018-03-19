0eb1a06c53bc6bcb4e1fbb0865feaf7f6e396ff0
Tomotada Saiugusa <nirot1r@g-second.net>
2018-03-19 20:58:39 +0900

    VOEZスコアを抽出するプログラム

---
+++ voez/voezscore.js
@@ -0,0 +1,9 @@
+const voezscore = function(msg) {
+    const regexScore = /.*\\n([0-9]+)\\n0再プレイ/;
+    let result;
+
+    if(!regexScore.test(msg)) return 0;
+    result = parseInt(msg.match(regexScore)[1]);
+
+    return result;
+}
